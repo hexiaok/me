0ea9947e8c0487e60ec2afcfced3880da1d4d2c8
Jian He <j.he@c02zl3rdlvdr.fritz.box>
2020-04-06 22:53:46 +0200

    üpdates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import './App.css';
 import ReactGlobe from 'react-globe';
+import Container from 'react-bootstrap/Container';
+import darkGlobe from './globe_dark.jpg'
+
+
+// import Greet from './components/greet';
+// import Welcome from './components/welcome';
+
 
 function App() {
   return (
     <div className="App">
-      <ReactGlobe
-        globeOptions={{
-          texture:
-            'https://raw.githubusercontent.com/chrisrzhou/react-globe/master/textures/globe_dark.jpg',
-        }}
-      />
+      {/* <header className="App-header">
+        <Greet />
+        <Welcome />
+      </header>
+       */}
+      <Container fluid>
+        <div class="row" style={{ height: "100vh" }}>
+          <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#ffffff"
+          }}
+            class="col-lg-6">
+            <h1 style={{ padding: "10%", lineHeight: "160%" }}>Jian He<br />Designer, living in Berlin.</h1>
+          </div>
+
+          <div class="col-lg-6" style={{ display: "flex", alignItems: "center" }}>
+            <div style={{ height: "50%", width:"100%" }}>
+              <ReactGlobe
+                globeOptions={{
+                  texture: darkGlobe,
+                  // backgroundTexture:'',
+                  // cloudsTexture:'',
+                  enableBackground: false,
+                  enableClouds: false,
+                  enableGlow: false
+                }}
+                cameraOptions={{
+                  enableZoom: false,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+      </Container>
+
+
     </div>
   );
 }
 
 export default App;
+
