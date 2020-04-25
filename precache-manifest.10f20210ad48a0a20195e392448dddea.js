0e3d5348f27150d9926240ae0e8da07958e85ae0
Jian He <j.he@c02zl3rdlvdr.fritz.box>
2020-04-26 00:36:38 +0200

    Updates

---
+++ precache-manifest.10f20210ad48a0a20195e392448dddea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e156c6dc20118057c0d23c1d976d6989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0efc3a449eb979574b2b",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "37d0267567f25be80572",
+    "url": "/static/css/main.ae17115a.chunk.css"
+  },
+  {
+    "revision": "0efc3a449eb979574b2b",
+    "url": "/static/js/2.0c281ef5.chunk.js"
+  },
+  {
+    "revision": "d2f71af55cff0b1df87b6d3980e03a41",
+    "url": "/static/js/2.0c281ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d0267567f25be80572",
+    "url": "/static/js/main.b1720727.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
