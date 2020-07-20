fc0b495a9cd5fd3185ff5b2f3275d6ddd3f99e1c
Jian He <j.he@c02zl3rdlvdr.fritz.box>
2020-07-20 22:58:14 +0200

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [click, setClick] = useState(0)
   return (
     <div className="App" onMouseDown={() => setClick(!click)}>
+
       <Container fluid>
         <div className="row" style={{ height: "100vh" }}>
           <div style={{
@@ -17,10 +18,13 @@ function App() {
             backgroundColor: "#ffffff"
           }}
             className="col-xl-6">
-            <h1 style={{fontSize: 28, paddingLeft: "6%", paddingRight: "6%",lineHeight: "160%" }}>Jian He<br />Designer now based in Berlin</h1>
+            <div>
+            <h1 style={{fontSize: 40}}>Jian He</h1>
+            <p style={{fontSize: 24}}>Designer based in Berlin </p>
+            </div>  
           </div>
           <div className="col-xl-6" style={{ display: "flex", alignItems: "center" }}>
-            <div style={{ height: "60%", width: "100%" }}>
+            <div style={{ height: "50%", width: "100%" }}>
               <ReactGlobe
                 globeOptions={{
                   enableBackground: false,
@@ -41,10 +45,35 @@ function App() {
                   getTooltipContent: marker => `${marker.city} ${marker.year} <br> ${marker.activity}`,
                   radiusScaleRange: [0.001, 0.045],
                 }}
-              />
+              /> 
             </div>
           </div>
         </div>
+   
+    
+       {/* A JSX comment */}         
+
+    <div className="row" style={{ height: "100vh" }}>
+
+      <div className="col-xl-6">
+      
+      </div>
+
+      <div 
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff"
+      }}
+      className="col-xl-6">
+       <div>
+       <p style={{fontSize: 24}}>Designer based in Berlin </p>
+        <p style={{fontSize: 24}}>Designer based in Berlin </p>
+        </div> 
+      </div>
+
+    </div>
 
       </Container>
     </div>
