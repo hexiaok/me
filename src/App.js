51d8af14f62f5806744ee7ed09920fab8cae817d
tgmof <>
2021-01-01 17:40:21 +0100

    Remove warnings and cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Globe from './Globe'
 import Bio from './Bio'
 import GlobeRight from './GlobeRight'
 import GlobeLeft from './GlobeLeft'
@@ -10,29 +9,26 @@ import JourneyContent from './JourneyContent'
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {moveGlobeToLeft: true}
-  }
-  
+  state = { moveGlobeToLeft: true }
+
   render() {
     return (
-    <div className="App">
-      <p style={{backgroundColor: "white"}} onClick={() => this.setState({moveGlobeToLeft: !this.state.moveGlobeToLeft})}>Click</p>
-      
-      <Container fluid>
-        <div className="row" style={{ height: "100vh" }}>
+      <div className="App">
+        <p style={{ backgroundColor: "white" }} onClick={() => this.setState({ moveGlobeToLeft: !this.state.moveGlobeToLeft })}>Click</p>
 
-        {this.state.moveGlobeToLeft === true ? <Bio/> : null}
-        {this.state.moveGlobeToLeft === true ? <GlobeRight /> : null}
+        <Container fluid>
+          <div className="row" style={{ height: "100vh" }}>
 
-        {this.state.moveGlobeToLeft === true ? null :<GlobeLeft />}
-        {this.state.moveGlobeToLeft === true ? null :<JourneyContent />}
-     
+            {this.state.moveGlobeToLeft === true ? <Bio /> : null}
+            {this.state.moveGlobeToLeft === true ? <GlobeRight /> : null}
 
-        </div>
-      </Container>
-    </div>
+            {this.state.moveGlobeToLeft === true ? null : <GlobeLeft />}
+            {this.state.moveGlobeToLeft === true ? null : <JourneyContent />}
+
+
+          </div>
+        </Container>
+      </div>
     )
   }
 }
