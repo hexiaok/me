5ccbfd8780d644dc0b0985121a28b3c4c380c689
Jian He <hexiaok@gmail.com>
2021-01-01 18:01:18 +0100

    update css

--- src/JourneyContent.js
+++ src/JourneyContent.js
@@ -1,20 +1,17 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
 function JourneyContent() {
 
-    return (
-      <div className="right-align col-xl-7 order-xl-2" style={{ padding: "8em" }}>
-        <div >
-          <h1 className="journey-header">
-            Product Designer
-            <br />
-            Berlin
-          </h1>
-        </div> 
-      </div> 
-    );
+  return (
+    <div className="col-xl-7 order-xl-2" style={{ padding: "8em" }}>
+      <div >
+        <h1 className="journey-header right-align">Product Designer</h1>
+        <h1 className="journey-header right-align">Berlin</h1>
+      </div>
+    </div>
+  );
 }
 
 export default JourneyContent;
